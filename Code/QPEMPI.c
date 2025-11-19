060999ec5a7bc2f544d80e6b0e08430e7fed834b
Austin Phalines <aphalines1@hpcl2-2.hslinux>
2025-11-19 14:56:14 -0500

    syntax fixes

--- Code/QPEMPI.c
+++ Code/QPEMPI.c
@@ -88,9 +88,9 @@ int main(int argc, char **argv) {
   }
   	int size;
 	int rank;
-	MPI_init(&size, &rank);
-	MPI_Comm_rank(&rank, MPI_COMM_WORLD);
-	MPI_Comm_size(&size, MPI_COMM_WORLD);
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	if (rank==0)
 	{
 		long lines = count_lines(filename);
@@ -481,7 +481,7 @@ static int compare_attr_value(const CarInventory *car, const char *attr,
     return count;
 }
   }
-
+parallelize
   const char *lhs = NULL;
   if (strcasecmp(attr, "Model") == 0) {
     lhs = car->Model;
